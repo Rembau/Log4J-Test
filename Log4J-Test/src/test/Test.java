d972eefbbed9e25a2580183064e47ea47426e6da
Rembau <Rembau@Rembau-PC>
2013-02-21 10:56:02 +0800

    简单修改

--- Log4J-Test/src/test/Test.java
+++ Log4J-Test/src/test/Test.java
@@ -7,5 +7,11 @@ public class Test {
 		Logger  log = Logger.getLogger(Test.class.getName());
 		System.out.println(Test.class);
 		log.info("..");
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		log.debug("1111");
 	}
 }
