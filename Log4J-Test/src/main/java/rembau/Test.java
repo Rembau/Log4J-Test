2e0a9126d48912b7e8f9cbcac4822e3fde7a00c8
rembau <504229406@qq.com>
2016-10-09 17:23:35 +0800

    重构

---
+++ Log4J-Test/src/main/java/rembau/Test.java
@@ -0,0 +1,19 @@
+package rembau;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+    private final static Logger logger = LoggerFactory.getLogger(Test.class);
+
+    public static void main(String[] args) {
+        logger.info("..");
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+            logger.error("", e);
+        }
+        logger.debug("1111");
+	}
+}
